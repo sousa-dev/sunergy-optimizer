3132580fe838ddfe2780728b0d33c91acf949ed8
hc-sousa <henrique@sousadev.com>
2024-03-11 11:59:43 -0100

    Update Every 15 minutes

--- __init__.py
+++ __init__.py
@@ -112,7 +112,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     first_update.replace(second=0, microsecond=0)
     hass.async_create_task(initial_update(first_update=now + datetime.timedelta(minutes=TIME_INTERVAL)))
 
-    async_track_time_interval(hass, update_state, datetime.timedelta(minutes=1))
+    async_track_time_interval(hass, update_state, datetime.timedelta(minutes=15))
 
     return True
 
